24530a885a4a39336aa456b0c2b2725b649e9315
Pippcicle <pippaamsmith@gmail.com>
2025-03-13 18:18:21 +0000

    Add files via upload

--- colourchange.py
+++ colourchange.py
@@ -8,8 +8,31 @@ def add_item():
     listbox.insert(END, txt_input.get())
     txt_input.delete(0,END)
 
+def delete_item(): 
+    index = listbox.curselection()
+    if index :
+        listbox.delete(index)
 
-save_btn = Button(window, text = 'SAVE COLOURS', width = 15, bg = '#242038', fg = '#CAC4CE')
+def save_colour():
+    index = listbox.curselection()
+    for i in index : 
+
+        if listbox.get(i) == "Pink" :
+            window.configure(bg='pink')
+        elif listbox.get(i) == "Blue" :
+            window.configure(bg = 'blue')
+        elif listbox.get(i) == "Green" :
+            window.configure(bg = 'green')
+        elif listbox.get(i) == "Yellow" :
+            window.configure(bg = 'yellow')
+        elif listbox.get(i) == "Purple" :
+            window.configure(bg = 'purple')
+        elif listbox.get(i) == "Red" :
+            window.configure(bg = 'red')
+        elif listbox.get(i) == "Orange" :
+            window.configure(bg = 'orange')    
+
+save_btn = Button(window, text = 'SAVE COLOURS', width = 15, bg = '#242038', fg = '#CAC4CE', command = save_colour)
 save_btn.pack(padx = 5, pady = 5)
 txt_input = Entry(window,  width = 35, bg = '#8D86C9')
 txt_input.pack(padx = 5, pady = 5)
@@ -31,7 +54,5 @@ for color in colours:
 
 listbox.pack(side = LEFT, padx = 5)
 scrollbar.config(command = listbox.yview)
-
-
 
 window.mainloop()
